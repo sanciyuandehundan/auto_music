c64c593a57c69ac80e6670d89fa370c613e5556e
一 吴 <a0905591936@gmail.com>
2024-03-27 12:22:39 +0800

    1

--- auto_music.cpp
+++ auto_music.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 #include <sstream>
 #include <vector>
-#include <windows.h>
+#include <Windows.h>
 using namespace std;
 
 void read(string path);
@@ -19,6 +19,34 @@ void put_16(string &s);
 void put_16(int &i);
 int trans_16s_10int(string a);
 class Yingui;
+enum key {
+    A=0x41,
+    B=0x42,
+    C=0x43,
+    D=0x44,
+    E=0x45,
+    F=0x46,
+    G=0x47,
+    H=0x48,
+    I=0x49,
+    J=0x4a,
+    K=0x4b,
+    L=0x4c,
+    M=0x4d,
+    N=0x4e,
+    O=0x4f,
+    P=0x50,
+    Q=0x51,
+    R=0x52,
+    S=0x53,
+    T=0x54,
+    U=0x55,
+    V=0x56,
+    W=0x57,
+    X=0x58,
+    Y=0x59,
+    Z=0x5a,
+};
 //提前宣告
 
 int midi_head_long = 0;//文件头多长
@@ -55,7 +83,12 @@ public:
     class play_down :public play {
     public:
         int note;
+        int k;
+        INPUT input;
+        input.type = INPUT_KEYBOARD;
+
         void play_() {
+            //SendInput(k,0,);
             next->play_();
         }
         play_down(play* previous, int note_) {
